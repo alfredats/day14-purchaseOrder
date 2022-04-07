eb9c83be712e0b202f1c5fc321fdb580ee78b349
Alfred A <20140832+alfredats@users.noreply.github.com>
2022-04-07 22:26:49 +0800

    basic test
    add badges

---
+++ README.md
@@ -0,0 +1,4 @@
+# day14-purchaseOrder
+
+[![Compile, Test, Deploy](https://github.com/alfredats/day14-purchaseOrder/actions/workflows/onPush.yaml/badge.svg)](https://github.com/alfredats/day14-purchaseOrder/actions/workflows/onPush.yaml)
+![Coverage](.github/badges/jacoco.svg)
--- src/test/java/visa/vttp/paf/day14purchaseOrder/Day14PurchaseOrderApplicationTests.java
+++ src/test/java/visa/vttp/paf/day14purchaseOrder/Day14PurchaseOrderApplicationTests.java
@@ -1,13 +1,33 @@
 package visa.vttp.paf.day14purchaseOrder;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import visa.vttp.paf.day14purchaseOrder.repo.KwikmartRepo;
+
 
 @SpringBootTest
 class Day14PurchaseOrderApplicationTests {
 
+	@Autowired
+	private KwikmartRepo kRepo;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void hasSKU_noSuchSKU() {
+		assertFalse(kRepo.hasSKU(9999));
+	}
+
+	@Test 
+	void hasSKU_skuExists() {
+		assertTrue(kRepo.hasSKU(2));
 	}
 
 }
